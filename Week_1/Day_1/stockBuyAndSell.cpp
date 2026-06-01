06d3da9dcd47489789982428fbf55fd5f89a1cc9
MrUnknwon-47 <vaibhavsingh8d36@gmail.com>
2026-06-01 17:35:50 +0530

    3. Best Time to Buy and Sell Stock

---
+++ Week_1/Day_1/stockBuyAndSell.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit= 0, bestBuy= prices[0];
+
+        for(int i= 1; i< prices.size(); i++) {
+            if(prices[i]>bestBuy) maxProfit= max(maxProfit, prices[i]-bestBuy);
+            bestBuy= min(bestBuy, prices[i]);
+        }
+        return maxProfit;
+    }
+};
+
+int main(){
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+
+   Solution sol;
+   int result = sol.maxProfit(prices);
+
+
+   cout << "Maximum Profit: " << result << endl;
+
+    return 0;
+}
